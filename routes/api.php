a744b883d82a0df69a3b13b58c6048a25c41e9fc
Lorenzo <lorenzocardoso@hotmail.com>
2021-09-30 08:57:49 -0300

    Adicioanndo rotas de autenticação e autorização e o AuthController

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Rotas de autenticação
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('me', 'App\Http\Controllers\AuthController@me');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
